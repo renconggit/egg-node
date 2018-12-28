684f41d50a9b29b93f05d7816174cf1dcfcc7a15
865506898@qq.com <865506898@qq.com>
2018-12-28 11:33:27 +0800

    ss

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,7 +4,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, eggssssss';
+    this.ctx.body = 'hi,';
   }
 }
 
